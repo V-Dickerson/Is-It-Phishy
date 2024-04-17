3f999a9640b82f1ef61d4fd3f655366a7c0f1711
Vanessa Dickerson <van.dickerson@tamu.edu>
2024-04-17 09:35:35 -0500

    UI changes and integrations. Added answer screen where you can see the correct answer, what the model picked, and its confidence. Added context to the score component.

--- go-phish/components/KeepingScore.tsx
+++ go-phish/components/KeepingScore.tsx
@@ -8,8 +8,18 @@ export default function KeepingScore(props) {
                 <Heading size='lg'>Scores</Heading>
             </CardHeader>
             <CardBody>
-                <Text>Computer: {props.model_correct}/{props.total_rounds}</Text>
-                <Text>Human: {props.usr_correct}/{props.total_rounds}</Text>
+                {props.model_correct > props.usr_correct &&
+                    <Text as='b'>The neural network is winning!</Text>
+                }
+                {props.model_correct < props.usr_correct &&
+                    <Text as='b'>You're beating the neural network!</Text>
+                }
+                {props.model_correct == props.usr_correct &&
+                    <Text as='b'>You're tied with the neural network!</Text>
+                }
+
+                <Text align='center'>Computer: {props.model_correct}/{props.total_rounds}</Text>
+                <Text align='center'>Human: {props.usr_correct}/{props.total_rounds}</Text>
             </CardBody>
         </Card>
     )
--- go-phish/pages/play.tsx
+++ go-phish/pages/play.tsx
@@ -1,8 +1,9 @@
-import { Flex, VStack, Card, CardHeader, Heading, CardBody, CardFooter, ButtonGroup, Button, Box, Text, useDisclosure, Modal, ModalHeader, ModalCloseButton, ModalBody, ModalOverlay, ModalContent, ModalFooter, TableContainer, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react"
+import { Flex, VStack, Card, CardHeader, Heading, CardBody, CardFooter, ButtonGroup, Button, Box, Text, useDisclosure, Modal, ModalHeader, ModalCloseButton, ModalBody, ModalOverlay, ModalContent, ModalFooter, TableContainer, Table, Thead, Tbody, Tr, Th, Td, Container } from "@chakra-ui/react"
 import { propagateServerField } from "next/dist/server/lib/render-server";
 import { Divider } from "@chakra-ui/react"
 import { useEffect, useState } from "react";
 import KeepingScore from "../components/KeepingScore";
+import { useRouter } from "next/router";
 
 
 const Play = () => {
@@ -22,6 +23,8 @@ const Play = () => {
     const [model_correct, setModelCorrect] = useState(0);
     const [total_rounds, setTotalRounds] = useState(0);
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const [asking_question, setAskingQuestion] = useState(true);
+    const router = useRouter();
 
     const [data, setData] = useState({
         url: "",
@@ -31,76 +34,145 @@ const Play = () => {
         answer: -1,
     });
 
-    useEffect(() => {
+    function handleAnswer(response) {
+        console.log("handleAnswer called")
+        setTotalRounds(total_rounds + 1)
+        if (response == data.answer) {
+            setUsrCorrect(usr_correct + 1)
+        }
+        if (data.model_answer == data.answer) {
+            setModelCorrect(model_correct + 1)
+        }
+        setAskingQuestion(!asking_question)
+        getQuestion()
+    }
+
+    function getQuestion() {
         fetch('/api/get-question').then((res) =>
-        res.json().then((data) => {
-            setData({
-                url: data.url,
-                url_data: data.url_data,
-                model_answer: data.model_answer,
-                model_confidence: data.model_confidence,
-                answer: data.answer
-            });
-        }))
+            res.json().then((data) => {
+                setData({
+                    url: data.url,
+                    url_data: data.url_data,
+                    model_answer: data.model_answer,
+                    model_confidence: data.model_confidence,
+                    answer: data.answer
+                });
+            }))
+    }
+
+    useEffect(() => {
+        getQuestion()
     }, []);
 
-    return (
-        <Flex height="100vh" alignItems="center" justifyContent="center">
-            <VStack direction='column' spacing='24px'>
-                <Card align='center' minHeight='60vh' minWidth='50vh'>
-                    <VStack spacing='3vh' align='center'>
-                        <CardHeader alignItems='center'>
-                            <Heading size='lg' textAlign='center'> URL: </Heading>
-                            <Text align='center' fontSize='med' padding='1vh'>{data.url}</Text>
-                            <Divider minWidth='200px' />
-                        </CardHeader>
-                        <Button size='lg' textAlign='center' alignSelf='center' width='20vw' minWidth='200px' onClick={onOpen}>Model View...</Button>
+    function playScreen() {
+        return (
+            <Card align='center' minHeight='60vh' minWidth='50vh' maxW='lg'>
+                <VStack spacing='3vh' align='center'>
+                    <CardHeader alignItems='center'>
+                        <Heading size='lg' textAlign='center'> URL: </Heading>
+                        <Text align='center' fontSize='med' padding='1vh'>{data.url}</Text>
+                        <Divider minWidth='200px' />
+                    </CardHeader>
+                    <Button size='lg' textAlign='center' alignSelf='center' width='20vw' minWidth='200px' onClick={onOpen}>Model View...</Button>
 
-                        <Modal isOpen={isOpen} onClose={onClose} scrollBehavior='inside'>
-                            <ModalOverlay />
-                            <ModalContent>
-                                <ModalHeader>Model View</ModalHeader>
-                                <ModalCloseButton />
-                                <ModalBody>
-                                    <Text align='center'>This is what the model sees!</Text>
-                                    <Divider padding='1vh' />
-                                    <TableContainer>
-                                        <Table size='sm' variant='simple' alignSelf='center'>
-                                            <Thead>
-                                                <Tr>
-                                                    <Th>Descriptor</Th>
-                                                    <Th>Value</Th>
-                                                </Tr>
-                                            </Thead>
-                                            <Tbody>
+                    <Modal isOpen={isOpen} onClose={onClose} scrollBehavior='inside'>
+                        <ModalOverlay />
+                        <ModalContent>
+                            <ModalHeader>Model View</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody>
+                                <Text align='center'>This is what the model sees!</Text>
+                                <Divider padding='1vh' />
+                                <TableContainer>
+                                    <Table size='sm' variant='simple' alignSelf='center'>
+                                        <Thead>
+                                            <Tr>
+                                                <Th>Descriptor</Th>
+                                                <Th>Value</Th>
+                                            </Tr>
+                                        </Thead>
+                                        <Tbody>
                                             {
-                                                data.url_data.map(it => 
-                                                    <Tr>
+                                                data.url_data.map(it =>
+                                                    <Tr key={it[0]}>
                                                         <Td>{it[0]}</Td>
                                                         <Td>{it[1]}</Td>
                                                     </Tr>
                                                 )
                                             }
-                                            </Tbody>
-                                        </Table>
-                                    </TableContainer>
-                                </ModalBody>
-                                <ModalFooter>
-                                    <Button colorScheme='blue' onClick={onClose}>Close</Button>
-                                </ModalFooter>
-                            </ModalContent>
-                        </Modal>
+                                        </Tbody>
+                                    </Table>
+                                </TableContainer>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button colorScheme='blue' onClick={onClose}>Close</Button>
+                            </ModalFooter>
+                        </ModalContent>
+                    </Modal>
 
 
-                    </VStack>
-                    <CardFooter marginTop='20vh'>
-                        <ButtonGroup gap='5' width='inherit'>
-                            <Button colorScheme='green' size='lg' width='20vw' marginLeft='2vw'>Legit!</Button>
-                            <Button colorScheme='red' size='lg' width='20vw' marginRight='2vw'>Go Phish!</Button>
-                        </ButtonGroup>
-                    </CardFooter>
-                </Card>
-                <KeepingScore model_correct={model_correct} usr_correct={usr_correct} total_rounds={total_rounds}/>
+                </VStack>
+                <CardFooter marginTop='20vh'>
+                    <ButtonGroup gap='5' maxW='lg'>
+                        <Button colorScheme='green' size='lg' width='20vw' marginLeft='2vw' onClick={() => handleAnswer(false)}>Legit!</Button>
+                        <Button colorScheme='red' size='lg' width='20vw' marginRight='2vw' onClick={() => handleAnswer(true)}>Go Phish!</Button>
+                    </ButtonGroup>
+                </CardFooter>
+            </Card>
+        )
+    }
+
+    function answerScreen() {
+        return (
+            <Card align='center' minHeight='60vh' minWidth='50vh' maxW='lg' maxH='md'>
+                <VStack spacing='3vh' align='center' maxH='inherit'>
+                <CardHeader alignItems='center'>
+                    <Heading size='lg' textAlign='center'> The correct answer was: </Heading>
+                    <Heading size='md' textAlign='center' padding='1vh'>
+                        {data.answer == 1 &&
+                            "Phishing!"
+                        }
+                        {data.answer == 0 && 
+                            "Legit!"
+                        }
+                    </Heading>
+                    <Divider minWidth='200px' />
+                </CardHeader>
+                <CardBody>
+                    <Text align='center' fontSize='med' padding='1vh'>The computer guessed: </Text>
+                    <Text align='center' fontSize='med' padding='1vh'>
+                        {data.model_answer == 1 &&
+                            "Phishing"
+                        }
+                        {data.model_answer == 0 && 
+                            "Legit"
+                        }
+                    </Text>
+                    <Text align='center' fontSize='med' padding='1vh'>With a confidence level of: </Text>
+                    <Text align='center' fontSize='med' padding='1vh'>{data.model_confidence}%</Text>
+                </CardBody>
+                <CardFooter>
+                    <ButtonGroup gap='5' maxW='lg'>
+                        <Button colorScheme='blue' size='lg' width='20vw' marginLeft='2vw' onClick={() => {router.push('/')}} key="restart">Restart</Button>
+                        <Button colorScheme='blue' size='lg' width='20vw' marginRight='2vw' onClick={() => setAskingQuestion(!asking_question)} key="continue">Continue</Button>
+                    </ButtonGroup>
+                </CardFooter>
+                </VStack>
+                
+            </Card>
+        )
+    }
+
+    return (
+        <Flex height="100vh" alignItems="center" justifyContent="center">
+            <VStack direction='column' spacing='24px'>
+                {asking_question &&
+                    playScreen()
+                }
+                {!asking_question &&
+                    answerScreen()
+                }
+                <KeepingScore model_correct={model_correct} usr_correct={usr_correct} total_rounds={total_rounds} />
                 <Box ml='3'>
                     <Text fontWeight='bold'>
                         Made by Vanessa Dickerson
